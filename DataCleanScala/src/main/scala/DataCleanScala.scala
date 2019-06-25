e433b81f938e12889514bd6e0b9b90d0e4e18d82
tdjysu <albert_joe@foxmail.com>
2019-06-26 00:04:12 +0800

    ETL scala

---
+++ DataCleanScala/src/main/scala/DataCleanScala.scala
@@ -0,0 +1,88 @@
+import java.util.Properties
+
+import com.alibaba.fastjson.JSON
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.CheckpointingMode
+import org.apache.flink.streaming.api.environment.CheckpointConfig
+import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
+import org.apache.flink.streaming.util.serialization.KeyedSerializationSchemaWrapper
+import org.apache.flink.util.Collector
+import source.MyRedisSourceScala
+
+import scala.collection.mutable
+
+class DataCleanScala {
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//设置并行度为4
+    env.setParallelism(4)
+//    checkpoint 配置
+    env.enableCheckpointing(60000) //每60秒检查一次
+    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(30000) //最小检查间隔 30秒
+    env.getCheckpointConfig.setCheckpointTimeout(60000)
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+    env.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
+
+    //        指定kafka Source
+    val topic = "allData"
+    val brokerList = "localhost:9092"
+    val prop = new Properties
+    prop.setProperty("bootstrap.servers", brokerList)
+    prop.setProperty("group.id", "con2")
+    //      设置事务超时时间
+    prop.setProperty("transaction.timeout.ms", 60000 * 15 + "")
+
+    val myConsumer = new FlinkKafkaConsumer011[String]("hello",new SimpleStringSchema(),prop)
+    //获取kafka中的数据
+    val data = env.addSource(myConsumer)
+
+//   最新的国家码与大区的对应关系
+    val mapData = env.addSource(new MyRedisSourceScala).broadcast//可以把数据发送到后面的算子的所有并行实例中
+
+    val resData:DataStream[String] = data.connect(mapData).flatMap(new CoFlatMapFunction[String,mutable.Map[String,String],String] {
+      //存储国家和大区的关系
+      var allMsp = mutable.Map[String, String]()
+
+
+      override def flatMap1(value: String, out: Collector[String]) = {
+          val jSONObject = JSON.parseObject(value)
+          val dt = jSONObject.getString("dt")
+          val countryCode = jSONObject.getString("countryCode")
+//        从Redis中获取大区
+          val area = allMsp.get(countryCode)
+
+          val jsonArray = jSONObject.getJSONArray("data")
+          for(i <- 0 to jsonArray.size()){
+            val jsonObject1 =jsonArray.getJSONObject(i)
+            jsonObject1.put("area",area)
+            jsonObject1.put("dt",dt)
+            out.collect(jsonObject1.toString)
+          }
+      }
+
+      override def flatMap2(value: mutable.Map[String, String], out: Collector[String]) = {
+        this.allMsp = value
+      }
+
+      val outTopic = "allDataClean"
+      val outProp = new Properties()
+      outProp.setProperty("bootstrap.servers", "localhost:9092")
+      //      设置事务超时时间
+      outProp.setProperty("transaction.timeout.ms", 60000 * 15 + "")
+
+      val myProducer = new FlinkKafkaProducer011[String](outTopic, new KeyedSerializationSchemaWrapper[String](new SimpleStringSchema), outProp, FlinkKafkaProducer011.Semantic.EXACTLY_ONCE)
+
+      resData.addSink(myProducer)
+
+      env.execute("DataCleanScala")
+
+    })
+
+  }
+
+}
---
+++ DataCleanScala/src/main/scala/source/MyRedisSourceScala.scala
@@ -0,0 +1,56 @@
+package source
+
+import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
+import org.slf4j.LoggerFactory
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.exceptions.JedisConnectionException
+import scala.collection.mutable
+
+
+class MyRedisSourceScala extends RichSourceFunction[mutable.Map[String,String]] {
+  val logger = LoggerFactory.getLogger("MyRedisSourceScala")
+  val SLEEP_MILLION = 60000
+  var isRunning = true
+  var jedis:Jedis =null
+
+  override def run(ctx: SourceFunction.SourceContext[mutable.Map[String, String]]) = {
+    this.jedis = new Jedis("localhost",6379)
+    import scala.collection.JavaConversions.mapAsScalaMap
+    var keyValueMap = mutable.Map[String,String]()
+    while (isRunning){
+      try {
+        keyValueMap.clear()
+        keyValueMap = jedis.hgetAll("areas")
+
+        for (key <- keyValueMap.keys.toList) {
+          val value = keyValueMap.get(key).get
+          val splits = value.split(",")
+          for (split <- splits) {
+            keyValueMap += (key -> split)
+          }
+        }
+        if (keyValueMap.nonEmpty) {
+          ctx.collect(keyValueMap)
+        } else {
+          logger.warn("The Dim Data from Redis is Empty")
+        }
+        Thread.sleep(SLEEP_MILLION)
+      }catch {
+        case  e:JedisConnectionException => {
+          logger.error("Redis 链接获取异常,重新获取连接",e.getCause)
+          jedis = new Jedis("localhost",6379)
+        }
+        case  e:Exception => {
+          logger.error("Source 数据源异常",e.getCause)
+        }
+      }
+    }
+  }
+
+  override def cancel() = {
+    isRunning = false
+    if(jedis ！= null){
+      jedis.close()
+    }
+  }
+}
