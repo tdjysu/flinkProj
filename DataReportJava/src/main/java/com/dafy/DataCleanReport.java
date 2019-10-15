0b0da85c6bac28910d4e8a88a0106e5948256682
albert <qiaojingyang@dafy.com>
2019-10-15 22:03:26 +0800

    1015

--- DataReportJava/src/main/java/com/dafy/DataCleanReport.java
+++ DataReportJava/src/main/java/com/dafy/DataCleanReport.java
@@ -24,12 +24,13 @@ import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class DataReport {
+public class DataCleanReport {
 
-    Logger logger = LoggerFactory.getLogger(DataReport.class);
+    Logger logger = LoggerFactory.getLogger(DataCleanReport.class);
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 //       设置使用EventTime
@@ -44,7 +45,7 @@ public class DataReport {
         FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("localhost").setPort(6379).build();
 //        创建RedisSink
         RedisSink<Tuple3<String,String,String>> redisSink = new RedisSink<>(conf,new MyRedisMapper());
-
+//       kafka消费配置
         FlinkKafkaConsumer010 kafkaConsumer = new FlinkKafkaConsumer010<>(inTopic,new SimpleStringSchema(),prop);
 
         /**
@@ -128,6 +129,6 @@ System.out.println("事件时间--> " +" "+  evtime + "  Count--> " + count);
 //      将迟到的数据存储到Kafka
 //        sideOutput.addSink();
 //        resultData.addSink(redisSink);
-        env.execute(DataReport.class.getName());
+        env.execute(DataCleanReport.class.getName());
     }
 }
