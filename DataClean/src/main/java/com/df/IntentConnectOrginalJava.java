ed7be9d0079e255d71b49164287fca9a4f624b9a
albert <qiaojingyang@dafy.com>
2019-11-06 18:54:59 +0800

    1106

---
+++ DataClean/src/main/java/com/df/IntentConnectOrginalJava.java
@@ -0,0 +1,128 @@
+package com.df;
+
+import com.alibaba.fastjson.JSONObject;
+import com.df.DimSource.OrgaRedisSourceJava;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.RichCoFlatMapFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper;
+import org.apache.flink.util.Collector;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+public class IntentConnectOrginalJava {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+//        指定kafka Source
+        String topic = "intent_n1";
+        String brokerList = "192.168.8.206:9092,192.168.8.207:9092,192.168.8.208:9092";
+        Properties prop = new Properties();
+        prop.setProperty("bootstrap.servers",brokerList);
+        prop.setProperty("group.id", "con1");
+//      设置事务超时时间
+        prop.setProperty("transaction.timeout.ms",60000*15+"");
+
+
+        //      checkpoint配置
+        env.enableCheckpointing(5000);//每5秒检查一次
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(30000);//最小检查间隔 30秒
+        env.getCheckpointConfig().setCheckpointTimeout(60000);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        FlinkKafkaConsumer010 myConsumer =  new FlinkKafkaConsumer010<String>(topic,new SimpleStringSchema(),prop);
+//        myConsumer.setStartFromLatest();
+//      获取kafka中的数据
+        DataStream data = env.addSource(myConsumer);
+
+//      从Redis中获取维度数据
+        DataStream<HashMap<String, String[]>> dimData = env.addSource( new OrgaRedisSourceJava()).broadcast();
+        // 两个流要想被连接在一块，要么两个流都是未分组的，要么都是分组的即keyed-都做了keyby操作；如果都做了keyby，「key的值必须是相同的」
+       DataStream<String> resData =  data.connect(dimData)
+                .flatMap(new ControlFunction());
+        String outTopic = "intent_n2";
+        Properties outProp = new Properties();
+        outProp.setProperty("bootstrap.servers", "192.168.8.206:9092,192.168.8.207:9092,192.168.8.208:9092");
+//      设置事务超时时间
+        outProp.setProperty("transaction.timeout.ms",60000*15+"");
+
+
+
+        FlinkKafkaProducer010<String> myProducer = new FlinkKafkaProducer010<String>(outTopic,new KeyedSerializationSchemaWrapper<String>(new SimpleStringSchema()),outProp);
+        resData.addSink(myProducer);
+        env.execute("StreamingConnectCheckJava Job");
+    }
+
+    public static class ControlFunction extends RichCoFlatMapFunction<String, HashMap<String,String[]>, String> {
+        // key的状态用Boolean值来保存，是被两个流共享的
+        // Boolean的blocked用于记住单词是否在control流中，而且这些单词会从streamOfWords流中被过滤掉
+        // 营业部维度关系
+        HashMap<String,String[]>  orgDimMap = new HashMap<String,String[]>();
+        @Override
+        public void open(Configuration config) {
+//            blocked = getRuntimeContext().getState(new ValueStateDescriptor<>("blocked", Boolean.class));
+        }
+
+        // control.connect(streamOfWords)顺序决定了control流中的元素会被Flink运行时执行flatMap1时传入处理；streamOfWords流中的元素会被Flink运行时执行flatMap2时传入处理
+        @Override
+        public void flatMap1(String control_value, Collector<String> out) throws Exception {
+//System.out.println(control_value);
+            JSONObject jsonObject = JSONObject.parseObject(control_value);
+            String deptcode = jsonObject.getString("strdeptcode");
+
+//          通过营业部编码获取其它组织机构信息
+            String[] orgArray = orgDimMap.get(deptcode);
+            String detpname = orgArray[0];
+            String busiAreaCode = orgArray[1];
+            String busiAreaName = orgArray[2];
+            String adminAreaCode = orgArray[3];
+            String adminAreaName = orgArray[4];
+            jsonObject.put("detpname",detpname);
+            jsonObject.put("busiAreaCode",busiAreaCode);
+            jsonObject.put("busiAreaName",busiAreaName);
+            jsonObject.put("adminAreaCode",adminAreaCode);
+            jsonObject.put("adminAreaName",adminAreaName);
+            JSONObject beforeRecord = jsonObject.getJSONObject("beforeRecord");
+            String oldDeptcode = null;
+            if(beforeRecord != null){
+                oldDeptcode = beforeRecord.getString("strdeptcode");
+            }
+            if(oldDeptcode != null) {
+                String oldOrgArray[] = orgDimMap.get(oldDeptcode);
+                String olddetpname = oldOrgArray[0];
+                String oldbusiAreaCode = oldOrgArray[1];
+                String oldbusiAreaName = oldOrgArray[2];
+                String oldadminAreaCode = oldOrgArray[3];
+                String oldadminAreaName = oldOrgArray[4];
+                beforeRecord.put("detpname", olddetpname);
+                beforeRecord.put("busiAreaCode", oldbusiAreaCode);
+                beforeRecord.put("busiAreaName", oldbusiAreaName);
+                beforeRecord.put("adminAreaCode", oldadminAreaCode);
+                beforeRecord.put("adminAreaName", oldadminAreaName);
+            }
+            jsonObject.put("beforeRecord",beforeRecord);
+            out.collect(jsonObject.toJSONString());
+// System.out.println(jsonObject.toJSONString());
+
+        }
+
+        @Override
+        public void flatMap2(HashMap<String,String[]> value, Collector<String> out) throws Exception {
+//            if (blocked.value() == null) {
+//                out.collect(data_value);
+//            }
+            this.orgDimMap = value;
+        }
+    }
+
+
+}
---
+++ DataReportScala/src/main/scala/com/dafy/IntentReportOriginalAccuScala.scala
@@ -0,0 +1,235 @@
+package com.dafy
+
+import java.text.SimpleDateFormat
+import java.util.Properties
+
+import com.alibaba.fastjson.JSON
+import com.dafy.bean.ReportDeptBean
+import com.dafy.sink.MysqlSink
+import com.dafy.watermark.IntentReportWatermarkScala
+import org.apache.flink.api.common.functions.FilterFunction
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor, ValueState, ValueStateDescriptor}
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.environment.CheckpointConfig
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.evictors.TimeEvictor
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.triggers.ContinuousProcessingTimeTrigger
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
+import org.apache.flink.util.Collector
+import org.slf4j.LoggerFactory
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  *
+  */
+object IntentReportAccuScala {
+
+  val Logger = LoggerFactory.getLogger("IntentReportScala")
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    //修改并行度
+    env.setParallelism(10)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    //checkPoint 设置
+    env.enableCheckpointing(60000)
+    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    env.getCheckpointConfig.setCheckpointTimeout(10000)
+    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(30000)
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+    env.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
+
+    //隐式转换
+    import org.apache.flink.api.scala._
+
+    val inTopic = "intent_t2"
+    val brokerList = "192.168.8.206:9092,192.168.8.207:9092,192.168.8.208:9092"
+    val prop = new Properties
+    prop.setProperty("bootstrap.servers", brokerList)
+    prop.setProperty("group.id", "report1")
+
+    val kafkaConsumer = new FlinkKafkaConsumer010[String](inTopic,new SimpleStringSchema(),prop)
+//  设置kafka从最新数据消费
+    kafkaConsumer.setStartFromLatest()
+    /**
+      * 获取Kafka中的数据
+      *
+      * {"nborrowmode":240,"strdeptcode":"034201701","busiAreaCode":"030000010","nstate":5,
+      * "strloandate":1514708501000,"userid":2261687,"adminAreaCode":"034300020","adminAreaName":"两湖区域",
+      * "lamount":1807303,"deptname":"微金武汉市营业部","busiAreaName":"华中中心"}
+      */
+    val kafkaData = env.addSource(kafkaConsumer)
+
+
+
+
+    val mapData: DataStream[ReportDeptBean] = kafkaData.map(kafkaline => {
+      var eventTime: Long = 0
+      var deptCode: String = ""
+      var deptName: String = ""
+      var busiAreaCode: String = ""
+      var busiAreaName: String = ""
+      var adminAreaCode: String = ""
+      var adminAreaName: String = ""
+      var fundcode: String = ""
+      var lendCnt: Integer = 0
+      var lamount: Integer = 0
+      val kafkaDataBean: ReportDeptBean = new ReportDeptBean
+      //      解析文本转换JSON
+      val jsonObject = JSON.parseObject(kafkaline)
+
+      generateIntentBean(kafkaDataBean, jsonObject)
+
+      kafkaDataBean
+})
+//过滤异常数据
+    val filterData: DataStream[ReportDeptBean] = mapData.filter(new FilterFunction[ReportDeptBean] {
+      override def filter(value: ReportDeptBean): Boolean = {
+        return value.eventTime >= 0 && value.intentState == 9
+      }
+    })
+
+
+//保存迟到太久的数据,
+//    scala 要引入 org.apache.flink.streaming.api.scala.OutputTag
+    val outputTag = new OutputTag[ReportDeptBean]("late-data"){}
+
+    val resultData = filterData.assignTimestampsAndWatermarks( new IntentReportWatermarkScala())
+      .keyBy(_.deptCode)//定义分组字段
+      .window(TumblingEventTimeWindows.of(Time.days(1)))//滚动统计1天的数据
+      .trigger(ContinuousProcessingTimeTrigger.of(Time.seconds(5)))//增加trigger,以一定的频率输出中间结果
+      .evictor(TimeEvictor.of(Time.seconds(0),true))//增加evictor是因为，每次trigger触发计算，窗口中的所有数据都会参与，所以数据会
+//      触发多次，比较浪费，加evictor驱逐已经计算过的数据,就不会重复计算了
+      .sideOutputLateData(outputTag)//处理迟到的数据
+      .process(function = new ProcessWindowFunction[ReportDeptBean, ReportDeptBean, String, TimeWindow] {
+      var lendCntState: ValueState[Integer] = _ //定义借款笔数计算状态变量
+      var lendAmtState: ValueState[Integer] = _ //定义借款金额计算状态变量
+      var userCntSate: MapState[String, String] = _ //定义借款人数计算状态变量
+
+
+      override def open(parameters: Configuration): Unit = {
+        userCntSate = getRuntimeContext.getMapState(new MapStateDescriptor[String, String]("userCntState", classOf[String], classOf[String]))
+        lendCntState = getRuntimeContext.getState[Integer](new ValueStateDescriptor[Integer]("lendCntState", classOf[Integer]))
+        lendAmtState = getRuntimeContext.getState[Integer](new ValueStateDescriptor[Integer]("lendAmtState", classOf[Integer]))
+      }
+
+      override def process(strkey: String, context: Context, elements: Iterable[ReportDeptBean], out: Collector[ReportDeptBean]): Unit = {
+
+
+        //获取分组字段信息
+        val deptcode: String = strkey
+        // 存储时间，获取最后数据的时间
+        var timeBuf = ArrayBuffer[Long]()
+        var deptName: String = ""
+        var busiAreaCode: String = ""
+        var busiAreaName: String = ""
+        var adminAreaCode: String = ""
+        var adminAreaName: String = ""
+        var fundcode: String = ""
+        var timeArray: Array[Long] = new Array[Long](0)
+        var lendAmount = 0
+        var userId = ""
+        val elementNode = elements.iterator
+        var opFlag:String = ""
+
+        try {
+          //          遍历全部窗口数据
+          while (elementNode.hasNext) {
+            var next: ReportDeptBean = elementNode.next()
+            lendAmount = next.lamount
+            var funder = next.fundcode
+            deptName = next.deptName
+            busiAreaCode = next.busiAreaCode
+            busiAreaName = next.busiAreaName
+            adminAreaCode = next.adminAreaCode
+            adminAreaName = next.busiAreaName
+            fundcode = next.fundcode
+            userId = next.userId
+            opFlag = next.opFlag
+            userCntSate.put(userId, null)
+
+            opFlag match {
+              case "U" => {
+                lendCntState.update(if(lendCntState.value()== null) 0 else lendCntState.value() + 1)
+                lendAmtState.update(if(lendAmtState.value() == null) 0 else lendAmtState.value() + lendAmount)
+              }
+              case "US" =>{
+                lendCntState.update(if(lendCntState.value()== null) 0 else lendCntState.value() - 1)
+                lendAmtState.update(if(lendAmtState.value() == null) 0 else lendAmtState.value() - lendAmount)
+              }
+              case  "I" => {
+                lendCntState.update(if(lendCntState.value()== null) 0 else lendCntState.value() + 1)
+                lendAmtState.update(if(lendAmtState.value() == null) 0 else lendAmtState.value() + lendAmount)
+              }
+            }
+
+          }
+
+          var userCount: Integer = 0;
+          val userIterator = userCntSate.keys().iterator()
+          while (userIterator.hasNext) {
+            userIterator.next()
+            userCount += 1
+          }
+          var levtime: Long = System.currentTimeMillis()
+          var evtime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(levtime)
+          //           组织结果数据
+println( "线程ID-> " +Thread.currentThread().getId  + " "  + evtime + " 营业部->" + deptcode + " " + deptName + " 中心-> " + busiAreaCode + " " + busiAreaName + " 区域-> " + adminAreaCode + " " + adminAreaName + " 资方-> "
+           + fundcode + " 借款笔数-> " + lendCntState.value() + " 借款金额-> " + lendAmtState.value() + " 借款人数-> " + userCount)
+          var res: ReportDeptBean = getResultIntentData(deptcode, lendCntState.value(), lendAmtState.value(), deptName, busiAreaCode, busiAreaName, adminAreaCode, adminAreaName, fundcode, levtime,userCount)
+          out.collect(res)
+        }catch{ case  e:Exception => {
+          Logger.error("",e.getCause)
+          println("Cause-->" + e.getCause);
+          println("Message-->" + e.getMessage)
+        }
+      }
+      }
+    })
+
+
+    //将结果数据输出到Mysql
+      resultData.addSink(new MysqlSink("upsert","deptReportAccu"))
+      env.execute(IntentReportAccuScala.getClass().getName)
+
+  }
+
+  private def getResultIntentData(deptcode: String, lendCnt: Int, lendAmt: Int, deptName: String, busiAreaCode: String, busiAreaName: String, adminAreaCode: String, adminAreaName: String, fundcode: String, levtime: Long,userCount:Integer) = {
+    var res: ReportDeptBean = new ReportDeptBean()
+    res.eventTime = levtime
+    res.deptCode = deptcode
+    res.deptName = deptName
+    res.busiAreaCode = busiAreaCode
+    res.busiAreaName = busiAreaName
+    res.adminAreaCode = adminAreaCode
+    res.adminAreaName = adminAreaName
+    res.fundcode = fundcode
+    res.lamount = lendAmt
+    res.lendCnt = lendCnt
+    res.userCnt = userCount
+    res
+  }
+
+  private def generateIntentBean(kafkaDataBean: _root_.com.dafy.bean.ReportDeptBean, jsonObject: _root_.com.alibaba.fastjson.JSONObject) = {
+    kafkaDataBean.eventTime = jsonObject.getLong("strloandate")
+    kafkaDataBean.deptCode = jsonObject.getString("strdeptcode")
+    kafkaDataBean.deptName = jsonObject.getString("detpname")
+    kafkaDataBean.busiAreaCode = jsonObject.getString("busiAreaCode")
+    kafkaDataBean.busiAreaName = jsonObject.getString("busiAreaName")
+    kafkaDataBean.adminAreaCode = jsonObject.getString("adminAreaCode")
+    kafkaDataBean.adminAreaName = jsonObject.getString("adminAreaName")
+    kafkaDataBean.fundcode = jsonObject.getString("nborrowmode")
+    kafkaDataBean.lamount = jsonObject.getInteger("lamount")
+    kafkaDataBean.userId = jsonObject.getString("userid")
+    kafkaDataBean.intentState = jsonObject.getInteger("nstate")
+    kafkaDataBean.opFlag = jsonObject.getString("opFlag")
+  }
+}
