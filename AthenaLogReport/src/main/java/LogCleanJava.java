dfb1f21b6219a71d48ad88f4a90f0f3aa50f7c86
albert <qiaojingyang@dafy.com>
2019-12-10 17:56:26 +0800

    1017

--- AthenaLogReport/src/main/java/LogCleanJava.java
+++ AthenaLogReport/src/main/java/LogCleanJava.java
@@ -4,7 +4,11 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.RichCoFlatMapFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper;
+import org.apache.flink.util.Collector;
 
 import java.util.HashMap;
 import java.util.Properties;
@@ -17,15 +21,18 @@ import java.util.Properties;
  */
 public class LogCleanJava {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         String topic = "athena_t1";
+        String outTopic = "athena_o1";
         String brokerList = "192.168.8.206:9092,192.168.8.207:9092,192.168.8.208:9092";
         Properties prop = new Properties();
         prop.setProperty("bootstrap.servers",brokerList);
         prop.setProperty("group.id","ana1");
 //设置事务超时时间
         prop.setProperty("transaction.timeout.ms",60000*15+"");
+
+        Properties outProp = new Properties();
 
 
         //      checkpoint配置
@@ -42,8 +49,32 @@ public class LogCleanJava {
         DataStream kafkalog = env.addSource(kafkaConsumer);
 
         DataStream<HashMap<String,String>> funcDim = env.addSource(new FuncMysqlSourceJava()).broadcast();
+// 两个流要想被连接在一块，要么两个流都是未分组的，要么都是分组的即keyed-都做了keyby操作；如果都做了keyby，「key的值必须是相同的」
+        DataStream<String> resData = kafkalog.connect(funcDim).flatMap(new FuncControlFunction())
+                                        ;
 
+        outProp.setProperty("bootstrap.servers",brokerList);
+        outProp.setProperty("transaction.timeout.ms",60000*15+"");
+
+
+
+        FlinkKafkaProducer010<String> myProducer = new FlinkKafkaProducer010<String>(outTopic,new KeyedSerializationSchemaWrapper<String>(new SimpleStringSchema()),outProp);
+        resData.addSink(myProducer);
+        env.execute(LogCleanJava.class.getName());
     }
 
+
+    public static class FuncControlFunction extends RichCoFlatMapFunction<String, HashMap<String,String>, String> {
+
+        @Override
+        public void flatMap1(String value, Collector<String> out) throws Exception {
+
+        }
+
+        @Override
+        public void flatMap2(HashMap<String, String> value, Collector<String> out) throws Exception {
+
+        }
+    }
 
 }
